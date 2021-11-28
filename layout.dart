ddf4dc9765e4290c61379155960a6bec7a90f6cf
fltr-subrat <subratkishore143@gmail.com>
2021-11-28 18:19:26 +0530

    login stuff added

---
+++ layout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class layout extends StatefulWidget {
+  layout({Key? key}) : super(key: key);
+
+  @override
+  _layoutState createState() => _layoutState();
+}
+
+class _layoutState extends State<layout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
